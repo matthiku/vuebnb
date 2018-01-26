b5b0f78280379864a723660c2488a7c1b8c76dc2
Matthias Kuhs <matthiku@yahoo.com>
2018-01-26 15:15:25 +0000

    getting ready for Heroku deployment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,7 +13,14 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        /**
+         * ClearDB uses MySQL version 5.5, while our dev database is MySQL 5.7.
+         * The default string length in MySQL 5.5 is too short for 
+         * Passport authorization keys, so we need to manually set the 
+         * default string length in the app service provider 
+         * before we run the database migrations in our production app
+         */
+        Schema::defaultStringLength(191);
     }
 
     /**
